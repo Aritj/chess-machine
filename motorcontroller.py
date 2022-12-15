68c8d7df028a1fe9ba9615e39b8c4e4961af97b8
Aritj <aritj@protonmail.com>
2022-12-15 13:13:11 +0000

    Work in progress

--- motorcontroller.py
+++ motorcontroller.py
@@ -1,72 +1,94 @@
 import pigpio
 from time import sleep
 
+FILE_MAP = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8
+}
 
-class StepperMotorController:
-    PULSES = 800
-    DIR_PIN = 20
-    STEP_PIN = 21
+class StepperMotor:
+    def __init__(self, dir_pin, step_pin, pulses_hz = 800):
+        self.DIR_PIN = dir_pin
+        self.STEP_PIN = step_pin
+        self.PULSES_HZ = pulses_hz
+        self.SLEEP_TIMER = 200 / pulses_hz
 
-    def __init__(self):
         self.pi = pigpio.pi()
         self.pi.set_mode(self.DIR_PIN, pigpio.OUTPUT)
         self.pi.set_mode(self.STEP_PIN, pigpio.OUTPUT)
-        self.pi.set_PWM_dutycycle(self.STEP_PIN, 128)  # PWM 1/2 On 1/2 Off
-        self.pi.set_PWM_frequency(self.STEP_PIN, self.PULSES)  # pulses per second
+        self.pi.set_PWM_frequency(self.STEP_PIN, self.PULSES_HZ)  # pulses per second
+
+    def rotate(self, rotations) -> None:
+        self.pi.write(self.DIR_PIN, 1 if rotations < 0 else 0) # direction
+        self.pi.set_PWM_dutycycle(self.STEP_PIN, 128) # PWM on
+        sleep(self.SLEEP_TIMER * abs(rotations))
+        self.pi.set_PWM_dutycycle(self.STEP_PIN, 0)  # PWM off
+
+    def close(self) -> None:
+        self.pi.stop()
+
+class StepperMotorController:
+    __SCALING_VARIABLE: int = 4
+    __motors: list[StepperMotor] = []
+    __current_file: int = 0
+    __current_rank: int = 0
+
+    def __init__(self):
+        self.__file_motor = StepperMotor(20, 21)
+        #self.__rank_motor = StepperMotor(?, ?)
+        self.__motors.append(self.__file_motor)
+        #self.__motors.append(self.__rank_motor)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.pi.set_PWM_dutycycle(self.STEP_PIN, 0)  # PWM off
-        self.pi.stop()
-
-    def __position_x(self, x: int) -> None:
-        # TODO: map file to x position and set
-        pass
-
-    def __position_y(self, y: int) -> None:
-        # TODO: map rank to y position and set
-        pass
-
-    def __position_z(self, z: int) -> None:
-        # TODO: set z positions
-        pass
+        for motor in self.__motors:
+            motor.close()
 
     def __position_reset(self) -> None:
-        pass
+        self.__file_motor.rotate(-self.__current_file * self.__SCALING_VARIABLE)
+        #self.__rank_motor.rotate(-self.__current_rank * self.__SCALING_VARIABLE)
 
-    def __position_file_and_rank(self, file: str, rank: str) -> None:
-        
-        pass
+    def __position_file_and_rank(self, file: int, rank: int) -> None:
+        file_rotations: int = (file - self.__current_file) * self.__SCALING_VARIABLE
+        rank_rotations: int = (rank - self.__current_rank) * self.__SCALING_VARIABLE
+
+        self.__file_motor.rotate(file_rotations)
+        #self.__rank_motor.rotate(rank_rotations)
+
+        self.__current_file = file
+        self.__current_rank = rank
+
 
     def __grab(self) -> None:
-        # TODO: lower magnet, activate electro-magnet, lift magnet
-        pass
+        # TODO: activate electro-magnet
+        sleep(0.5)
 
     def __release(self) -> None:
-        # TODO: lower magnet, deactivate electro-magnet, lift magnet
-        pass
+        # TODO: deactivate electro-magnet
+        sleep(0.5)
 
     def move(self, file_from: str, rank_from: str, file_to: str, rank_to: str) -> None:
-        self.test(int(rank_from), int(rank_to))
-        print(f'MOVE |{file_from + rank_from}| TO |{file_to + rank_to}|')
-        pass
-        self.__position_file_and_rank(file_from, rank_from)     # step 1
-        self.__grab()                                           # step 2
-        self.__position_file_and_rank(file_to, rank_to)         # step 3
-        self.__release()                                        # step 4
-        self.__position_reset()                                 # step 5
-
-    def test(self, rank_from: int, rank_to: int):
-        for _ in range(abs(rank_from - rank_to)):
-            sleep(0.25)
-
+        self.__position_file_and_rank(FILE_MAP.get(file_from), int(rank_from))
+        self.__grab()
+        self.__position_file_and_rank(FILE_MAP.get(file_to), int(rank_to))
+        self.__release()
+        self.__position_reset()
 
 if __name__ == "__main__":
     # for unit testing purposes
     with StepperMotorController() as controller:
         controller.move(
-            "b", "1",
-            "a", "3"
+            "a", "1",
+            "d", "2"
         )
+    #motor = StepperMotor(20, 21)
+    #motor.rotate(-30)
+    #motor.close()
