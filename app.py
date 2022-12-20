55a3597d08e1314350edfdb83ad1186263fdf8ca
AriTJ <Ari.Joh20@student.setur.fo>
2022-12-20 23:48:26 +0000

    Changed /move status code from 200 to 204

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from json import loads
 
 app: Flask = Flask(__name__)
 
+
 @app.route('/move', methods=['POST'])
 def move() -> str:
     with StepperMotorController() as controller:
         controller.move(loads(request.data))
-    return "", 200
+    return '', 204
+
 
 @app.route('/', methods=['GET'])
 def index() -> str:
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
